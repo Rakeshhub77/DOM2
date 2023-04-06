63bf04c95d95b021071b0209865382436adb0a5b
Rakeshhub <fl2rakesh77@gmail.com>
2023-04-07 00:49:49 +0530

    task

--- dom.js
+++ dom.js
@@ -17,14 +17,23 @@
 
 //itemAdd.style.fontWeight= "bold"
 
-var items = document.getElementsByClassName("list-group-item");
-//console.log(items);
-items[2].style.color ="Green"
+// var items = document.getElementsByClassName("list-group-item");
+// console.log(items);
+// items[2].style.color ="Green"
 
-for(i=0;i<=items.length;i++)
-{
-items[i].style.fontWeight="bold";
-}
+// for(i=0;i<=items.length;i++)
+// {
+// items[i].style.fontWeight="bold";
+// }
 
 
 
+// var listss =document.getElementsByClassName("list-of-item")i
+// console.log(listss);
+// listss[0].innerText="List of Mobiles"
+
+
+
+// var lii= document.getElementsByTagName("li");
+// lii[3].style.color="red";
+// lii[4].innerText="Final product"
