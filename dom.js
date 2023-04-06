d83fef0db6baaa91116f868e977ce54c85c066be
Rakeshhub <fl2rakesh77@gmail.com>
2023-04-06 23:58:52 +0530

    Java Code

---
+++ dom.js
@@ -0,0 +1,30 @@
+//console.dir(document);
+//console.log(document.url);
+//var headerTitle=document.getElementById("header-title");
+//console.log(headerTitle);
+//headerTitle.textContent ="Hello"
+//headerTitle.innerText="Success"
+
+
+//var header=document.getElementById("main-header");
+//header.style.border ="solid black"
+
+
+//var itemAdd=document.getElementsByClassName("title");
+//console.log(itemAdd);
+
+//itemAdd[0].style.color ="Green"
+
+//itemAdd.style.fontWeight= "bold"
+
+var items = document.getElementsByClassName("list-group-item");
+//console.log(items);
+items[2].style.color ="Green"
+
+for(i=0;i<=items.length;i++)
+{
+items[i].style.fontWeight="bold";
+}
+
+
+
