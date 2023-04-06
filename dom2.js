0ae8c69951d6dad10a971ad35c0a952e1be4e454
Rakeshhub <fl2rakesh77@gmail.com>
2023-04-07 01:51:17 +0530

    task6

---
+++ dom2.js
@@ -0,0 +1,23 @@
+// var secondItem=document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.backgroundColor ="Green"
+
+
+// var thirdItem=document.querySelector('.list-group-item:nth-child(3)');
+// thirdItem.style.visibility ="hidden"
+
+
+
+//   var lii=document.querySelectorAll(".list-group-item");
+//   lii[1].style.color="Green"
+
+
+
+// var odd=document.querySelectorAll("li:nth-child(odd)");
+// for(var i=0;i<odd.length;i++)
+// {
+// odd[i].style.backgroundColor ='Green'
+// }
+
+
+
+
